64578c190699ee0c066e906d4276e691a005249b
Richard Gajdosik <rich.gajdosik@gmail.com>
2023-11-26 09:02:17 +0100

    Rewrote whoe logic

--- analysis.py
+++ analysis.py
@@ -78,72 +78,59 @@ def load_data(filename: str) -> pd.DataFrame:
 
                         # Načtení dat z CSV souboru
                         with iz.open(csv_file) as f:
-                            data = pd.read_csv(f, encoding='cp1250', names=headers, sep=';', low_memory=False)
+
+                            data = pd.read_csv(f, encoding='cp1250', names=headers, sep=';', dtype=str)
                             data['region'] = region
 
                             # Sloučení s hlavním DataFrame
                             all_data = pd.concat([all_data, data], ignore_index=True)
 
-        # Resetování indexu a odstranění výchozího indexu
-        all_data.reset_index(drop=True, inplace=True)
         return all_data
     
 # Ukol 2: zpracovani dat
 
-def convert_to_float(df, float_cols):
-    for col in float_cols:
-        if df[col].dtype == 'object':
-            # Attempt to convert to float after replacing commas with dots
-            converted_col = df[col].str.replace(',', '.').apply(pd.to_numeric, errors='coerce')
-
-            # Find and print values that couldn't be converted
-            invalid_values = df[col][converted_col.isna() & df[col].notna()]
-            if not invalid_values.empty:
-                print(f"Invalid values in column '{col}':")
-                print(invalid_values)
-
-            df[col] = converted_col
-    return df
-
-def convert_columns_to_float(df, float_cols):
-    for col in float_cols:
-        # Attempt to convert to float, replacing non-convertible values with NaN
-        converted_col = pd.to_numeric(df[col], errors='coerce')
-
-        # Find and print values that couldn't be converted
-        invalid_values = df[col][converted_col.isna() & df[col].notna()]
-        if not invalid_values.empty:
-            print(f"Invalid values in column '{col}':")
-            print(invalid_values)
-
-        df[col] = converted_col
-    return df
-
 def parse_data(df: pd.DataFrame, verbose: bool = False) -> pd.DataFrame:
+    if verbose:
+        orig_size = sum(df.memory_usage(deep=True)) / 10 ** 6  # size in MB
+
+
+    categorical_columns = [
+        'p5a', 'p6', 'p7', 'p8', 'p9', 'p10', 'p11', 'p12', 'p13a', 'p13b', 'p13c', 
+        'p15', 'p16', 'p17', 'p18', 'p19', 'p20', 'p21', 'p22', 'p23', 'p24', 'p27', 
+        'p28', 'p29', 'p30', 'p31', 'p32', 'p33c', 'p33f', 'p33g', 'p35', 
+        'p36', 'p37', 'p39', 'p44', 'p45a', 'p47', 'p48a', 'p49', 'p50a', 'p50b', 'p51', 'p52', 
+        'p55a', 'p57', 'p58'
+    ]
+
+    float_cols = ['a', 'b', 'd', 'e', 'f', 'g', 'o']
+
     # Convert 'p2a' to date format
     df['date'] = pd.to_datetime(df['p2a'])
 
     # Convert columns to categorical data type
-    cat_cols = df.select_dtypes(include='object').columns.tolist()
-    cat_cols.remove('region')  # Keep 'region' as a regular object
-    for col in cat_cols:
-        df[col] = df[col].astype('category')
+    for col in categorical_columns:
+        if col in df.columns:
+            df[col] = df[col].astype('category')
 
     # Convert specified columns to float
-    float_cols = ["p37", "a", "b", "d", "e", "f", "g", "o", "n", "r", "s"]
+    for col in float_cols:
+        # Replace commas with dots for float conversion
+        df[col] = df[col].astype(str).str.replace(',', '.')
+        # Convert to float, setting errors to 'coerce' to handle any non-convertible values
+        df[col] = pd.to_numeric(df[col], errors='coerce')
+        # Optional: Print invalid values that could not be converted
+        invalid_values = df[df[col].isna()][col]
+    if not invalid_values.empty:
+        print(f"Invalid values in column '{col}':")
+        print(invalid_values)
     
-    df = convert_to_float(df, float_cols)
-
-    df = convert_columns_to_float(df, float_cols)
-
     # Remove duplicates based on 'p1'
     df = df.drop_duplicates(subset=['p1']) # Keep the first occurrence
+    
 
     # Print the size information
     if verbose:
-        orig_size = sum(df.memory_usage(deep=True)) / 10 ** 6  # size in MB
         print(f'orig_size={orig_size:.1f} MB')
-        # To calculate the new size, first create a copy and then recompute the size
         df = df.copy()
         new_size = sum(df.memory_usage(deep=True)) / 10 ** 6
         print(f'new_size={new_size:.1f} MB')
@@ -174,20 +161,21 @@ def plot_fault(df: pd.DataFrame, fig_location: str = None,
                show_figure: bool = False):
     pass
 
-
 if __name__ == "__main__":
     # zde je ukazka pouziti, tuto cast muzete modifikovat podle libosti
     # skript nebude pri testovani pousten primo, ale budou volany konkreni
     # funkce.
     df = load_data("data.zip")
+
     #df.to_pickle("dataframe.pkl")
-    # df.to_csv('data.csv', index=False, sep=';', encoding='utf-8')
+    #df.to_csv('data.csv', index=False, sep=';', encoding='utf-8')
     #df = pd.read_pickle("dataframe.pkl")
+
     df2 = parse_data(df, True)
 
-    plot_state(df2, "01_state.png")
-    plot_alcohol(df2, "02_alcohol.png", True)
-    plot_fault(df2, "03_fault.png")
+    #plot_state(df2, "01_state.png")
+    #plot_alcohol(df2, "02_alcohol.png", True)
+    #plot_fault(df2, "03_fault.png")
 
 # Poznamka:
 # pro to, abyste se vyhnuli castemu nacitani muzete vyuzit napr
