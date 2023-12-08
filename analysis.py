b284dc581ddfbc7233e274aac2b3f7db9d7187e2
Richard Gajdosik <rich.gajdosik@gmail.com>
2023-12-08 21:52:24 +0100

    Deleted comments, added ukol5, changedat to p2a

--- analysis.py
+++ analysis.py
@@ -46,12 +46,12 @@ def load_data(filename: str) -> pd.DataFrame:
 
     # Načtení hlavního ZIP souboru
     with zipfile.ZipFile(filename, 'r') as z:
-        print(f"Načítání hlavního ZIP souboru: {filename}")
+        #print(f"Načítání hlavního ZIP souboru: {filename}")
         # Seznam všech ZIP souborů uvnitř hlavního ZIPu
         zip_files = z.namelist()
 
         for zip_file in zip_files:
-            print(f"Zpracovávání ZIP souboru: {zip_file}")
+            #print(f"Zpracovávání ZIP souboru: {zip_file}")
             # Načtení každého ZIP souboru
             with z.open(zip_file) as inner_zip:
                 with zipfile.ZipFile(inner_zip) as iz:
@@ -59,22 +59,22 @@ def load_data(filename: str) -> pd.DataFrame:
                     csv_files = [f for f in iz.namelist() if f.endswith('.csv') and not f.startswith('CHODCI')]
 
                     for csv_file in csv_files:
-                        print(f"Zpracovávání souboru: {csv_file}")
+                        #print(f"Zpracovávání souboru: {csv_file}")
 
                         # Ignorování prázdných souborů
                         if csv_file.startswith('08') or csv_file.startswith('09') or \
                                 csv_file.startswith('10') or csv_file.startswith('11') or \
                                 csv_file.startswith('12') or csv_file.startswith('13'):
-                            print(f"Soubor {csv_file} je prázdný a bude vynechán.")
+                            #print(f"Soubor {csv_file} je prázdný a bude vynechán.")
                             continue
 
                         # Získání kódu regionu z názvu souboru
                         region_code = csv_file.split('.')[0][-2:]
                         region = next((key for key, value in regions.items() if value == region_code), None)
                         if region is None:
-                            print(f"Region code {region_code} not found in regions map.")
+                            #print(f"Region code {region_code} not found in regions map.")
                             continue
-                        print(f"Soubor {csv_file} patří do regionu: {region}")
+                        #print(f"Soubor {csv_file} patří do regionu: {region}")
 
                         # Načtení dat z CSV souboru
                         with iz.open(csv_file) as f:
@@ -95,7 +95,7 @@ def parse_data(df: pd.DataFrame, verbose: bool = False) -> pd.DataFrame:
 
 
     categorical_columns = [
-        'p5a', 'p6', 'p7', 'p8', 'p9', 'p10', 'p12', 'p13a', 'p13b', 'p13c', 'p14',
+        'p5a', 'p6', 'p7', 'p8', 'p9', 'p12', 'p13a', 'p13b', 'p13c', 'p14',
         'p15', 'p16', 'p17', 'p18', 'p19', 'p20', 'p21', 'p22', 'p23', 'p24', 'p27', 
         'p28', 'p33c', 'p33f', 'p33g', 'p34', 'p35', 
         'p36', 'p37', 'p39', 'p44', 'p45a', 'p47', 'p48a', 'p49', 'p50a', 'p50b', 'p51', 'p52', 
@@ -104,11 +104,11 @@ def parse_data(df: pd.DataFrame, verbose: bool = False) -> pd.DataFrame:
 
     float_cols = ['a', 'b', 'd', 'e', 'f', 'g', 'o']
 
-    int_cols = ['p2b', 'p11']
+    int_cols = ['p2b', 'p11', 'p10']
 
     # Convert 'p2a' to date format
-    df['date'] = pd.to_datetime(df['p2a'])
-
+    df['p2a'] = pd.to_datetime(df['p2a'])
+    
     # Convert columns to categorical data type
     for col in categorical_columns:
         if col in df.columns:
@@ -267,9 +267,66 @@ def plot_alcohol(df: pd.DataFrame, fig_location: str = None,
 # Ukol 5: Zavinění nehody v čase
 
 
-def plot_fault(df: pd.DataFrame, fig_location: str = None,
-               show_figure: bool = False):
-    pass
+def plot_fault(df: pd.DataFrame, fig_location: str = None, show_figure: bool = False):
+    
+    df = df.copy()
+    
+    # Kontrolní tiskový výstup
+    print("Původní velikost dat:", df.shape)
+
+    # Select four regions
+    selected_regions = ['PHA', 'STC', 'JHC', 'PLK']
+    df_selected = df[df['region'].isin(selected_regions)]
+    print("Velikost dat po výběru regionů:", df_selected.shape)
+
+    # Filter the accidents based on cause (p10) and consider only values 1, 2, 3, and 4
+    causes = {
+        1: 'řidičem motorového vozidla',
+        2: 'řidičem nemotorového vozidla',
+        3: 'chodcem',
+        4: 'lesní zvěří, domácím zvířectvem'
+    }
+    df_selected = df_selected[df_selected['p10'].isin(causes.keys())]
+    df_selected['Cause'] = df_selected['p10'].map(causes)
+    print("Velikost dat po filtraci příčin:", df_selected.shape)
+
+    # Transform the table to have the count of accidents for each day and cause
+    pivot = df_selected.pivot_table(index='date', columns='Cause', values='p1', aggfunc='count', fill_value=0)
+    print("Data po pivot transformaci:", pivot.head())
+
+    # Resample to monthly level and stack the data
+    monthly_data = pivot.resample('M').sum().stack().reset_index(name='Počet nehod')
+    monthly_data['date'] = monthly_data['date'].dt.to_period('M')
+    print("Data po měsíčním resamplingu:", monthly_data.head())
+
+    # Plot the line chart
+    g = sns.relplot(
+        data=monthly_data,
+        x='date', y='Počet nehod',
+        hue='Cause', kind='line',
+        aspect=2, height=5,
+        facet_kws={'sharey': False, 'sharex': True}
+    )
+
+    # Nastavení os a titulků
+    g.set(xlim=(pd.Timestamp('2016-01-01'), pd.Timestamp('2023-01-01')))
+    g.set_titles("{col_name}")
+    g.set_axis_labels("Datum", "Počet nehod")
+    g.fig.suptitle('Zavinění nehody v čase', fontsize=16)
+
+    # Nastavení pozadí a mřížky
+    for ax in g.axes.flatten():
+        ax.yaxis.grid(True)
+        ax.xaxis.grid(False)
+        ax.set_facecolor('lightgray') # Nastavení barvy pozadí
+
+    # Uložení grafu do souboru
+    if fig_location:
+        g.savefig(fig_location)
+
+    # Zobrazení grafu
+    if show_figure:
+        plt.show()
 
 if __name__ == "__main__":
     # zde je ukazka pouziti, tuto cast muzete modifikovat podle libosti
